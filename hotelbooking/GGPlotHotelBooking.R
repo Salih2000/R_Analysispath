21ded7d75787b4069288de9918c510c354cb3cfb
Salih Abdullah Kilic <salihabdullahkilic@hotmail.com>
2023-06-26 20:38:33 +0300

    file fix

--- hotelbooking/GGPlotHotelBooking.R
+++ hotelbooking/GGPlotHotelBooking.R
@@ -11,3 +11,43 @@ ggplot(data = hotel_bookings) +
 
 ggplot(data = hotel_bookings) +
   geom_point(mapping = aes(x = lead_time, y = children))
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = market_segment)) +
+  facet_wrap(~hotel) +
+  labs(title="")
+
+min(hotel_bookings$arrival_date_year)
+
+max(hotel_bookings$arrival_date_year)
+
+
+mindate <- min(hotel_bookings$arrival_date_year)
+maxdate <- max(hotel_bookings$arrival_date_year)
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = market_segment)) +
+  facet_wrap(~hotel) +
+  theme(axis.text.x = element_text(angle = 45)) +
+  labs(title="Comparison of market segments by hotel type for hotel bookings",
+       subtitle=paste0("Data from: ", mindate, " to ", maxdate))
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = market_segment)) +
+  facet_wrap(~hotel) +
+  theme(axis.text.x = element_text(angle = 45)) +
+  labs(title="Comparison of market segments by hotel type for hotel bookings",
+       caption=paste0("Data from: ", mindate, " to ", maxdate))
+
+
+ggplot(data = hotel_bookings) +
+  geom_bar(mapping = aes(x = market_segment)) +
+  facet_wrap(~hotel) +
+  theme(axis.text.x = element_text(angle = 45)) +
+  labs(title="Comparison of market segments by hotel type for hotel bookings",
+       caption=paste0("Data from: ", mindate, " to ", maxdate),
+       x="Market Segment",
+       y="Number of Bookings")
+
+
